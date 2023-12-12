40b50f73e7d6ae08c53004b51a26cd21eb89c3fd
avbre <avbre@LAPTOP-F04Q9PCE>
2023-12-12 19:49:47 +0100

    new

--- src/main/java/com/mycompany/menuresorts/MenuActividades.java
+++ src/main/java/com/mycompany/menuresorts/MenuActividades.java
@@ -1,14 +1,19 @@
 
 package com.mycompany.menuresorts;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author avbre
  */
 public class MenuActividades {
+    static resort resort=new resort();
      public static void menu_actividades() {
-        int opcion=4;
-        while(opcion==4){
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        do{
             System.out.println("");
             System.out.println("MENÚ PRINCIPAL");
             System.out.println("1. Añadir actividad");
@@ -16,13 +21,19 @@ public class MenuActividades {
             System.out.println("3. Mostrar activida");
             System.out.println("0. Volver atrás");
             String s=MyInput.readString(); //almacena lo que lee en cadena tipo String y lo pasa a int
+            
+            System.out.print("Introduce una opcion: ");
             try{  
-                   opcion= Integer.parseInt(s); //el try intenta pasarlo a int
-            }catch(NumberFormatException ex){  //si no se consigue pasar a int
-                      System.out.println("La entrada no tiene formato de número. Inténtelo de nuevo");
-                      opcion=4;
+                   opcion= sc.nextInt(); //el try intenta pasarlo a int
+            }catch(InputMismatchException e){  //si no se consigue pasar a int
+                      System.out.println("No has introducido un numéro.");
+                      opcion= -1;
             } 
-            if((opcion>=0)&&(opcion<=3)){
+            if((opcion<0 || opcion >6){
+                System.out.println("La opcion no es valda.");
+            } else if (opcion == 0){
+                System.out.println("Saliendo del progrma...");
+            }else{    
                 switch(opcion){
                     case 1:
                         
@@ -43,11 +54,7 @@ public class MenuActividades {
                    
                 }
             }
-            else if ((opcion<0)||(opcion>3)){
-                    System.out.println("opción no válida. Inténtelo de nuevo");
-                    opcion=4;
-            }
-        }
+        }while (opcion !=0);
     }
 
 }
